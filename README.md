c50dc5d75425aa5c19147f8c0b9d6bb51fd2f5bf
Sylvie Melin <sy.melin@gmail.com>
2023-08-31 17:29:48 +0200

    main file

---
+++ README.md
@@ -0,0 +1,56 @@
+# Pong Python
+
+The Pong Python project is a simple implementation of the classic Pong game using Python and the Pygame library.
+
+## 1. General Information
+
+This project was created as a practicing exercise by following a video tutorial.
+The code used in this project is based on the code provided in the tutorial.
+See the [Acknowledgments](#6-acknowledgments) section for more information.
+
+## 2. Technologies
+
+- Python 3.11.4
+- Pygame 2.5.1
+
+## 3. Installation
+
+### 3.1 Clone the repository
+
+```bash
+https://github.com/SyMelin/Pong-Python.git
+```
+
+### 3.2 Install Pygame
+
+```bash
+pip install pygame==2.5.1
+```
+
+## 4. Usage
+
+### 4.1 Run the game
+
+```bash
+python main.py
+```
+
+### 4.2 Game controls
+
+- Player 1: Z and S keys
+- Player 2: Up and Down arrow keys
+
+### 4.3 Game rules
+
+- The game is played by two players
+- The ball bounces off the top and bottom walls and the paddles
+- The paddles move only vertically and cannot move beyond the top and bottom walls
+
+## 5. License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## 6. Acknowledgments
+
+- The tutorial used for this project is [Pong with Python & Pygame – Tutorial](https://www.youtube.com/watch?v=C6jJg9Zan7w) by [freeCodeCamp.org](https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ)
+- The Pygame library: [Pygame](https://www.pygame.org/news)
---
+++ game.py
@@ -0,0 +1,105 @@
+import pygame
+
+
+def run_game():
+    pygame.init()
+    # INITIALS
+    width, height = 1000, 600
+    wn = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pong Python")
+    run = True
+
+    # colors
+    pink = (255, 16, 240)
+    aqua = (0, 253, 255)
+    black = (0, 0, 0)
+
+    # ball parameters
+    radius = 15
+    ball_x, ball_y = width / 2 - radius, height / 2 - radius
+    ball_vel_x, ball_vel_y = 0.35, 0.35
+
+    # paddle parameters
+    paddle_width, paddle_height = 20, 120
+    left_paddle_y = right_paddle_y = height / 2 - paddle_height / 2
+    left_paddle_x, right_paddle_x = 100 - paddle_width / 2, width - (100 - paddle_width / 2)
+    right_paddle_vel = left_paddle_vel = 0
+
+    # main loop
+    while run:
+        # fill the screen with black
+        # so that the previous drawings are erased, the new position is drawn,
+        # and the human eye is tricked and can see the ball moving
+        wn.fill(black)
+
+        # all the events that happens in the pygame window will be stored in i
+        for i in pygame.event.get():
+            # if the user presses the quit button, the game will stop
+            if i.type == pygame.QUIT:
+                run = False
+            elif i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_UP:
+                    right_paddle_vel = -0.9
+                if i.key == pygame.K_DOWN:
+                    right_paddle_vel = 0.9
+                if i.key == pygame.K_z:
+                    left_paddle_vel = -0.9
+                if i.key == pygame.K_s:
+                    left_paddle_vel = 0.9
+
+            # check if the user releases the key
+            if i.type == pygame.KEYUP:
+                if i.key == pygame.K_UP or i.key == pygame.K_DOWN:
+                    right_paddle_vel = 0
+                if i.key == pygame.K_z or i.key == pygame.K_s:
+                    left_paddle_vel = 0
+
+        # movements
+        ball_x += ball_vel_x
+        ball_y += ball_vel_y
+        right_paddle_y += right_paddle_vel
+        left_paddle_y += left_paddle_vel
+
+        # ball's movements control
+        # if the ball hits the up or down wall, it will bounce
+        if ball_y <= 0 + radius or ball_y >= height - radius:
+            ball_vel_y *= -1
+        # if the ball hits the left or right wall, the ball will be reset to the center
+        if ball_x >= width - radius:
+            ball_x, ball_y = width / 2 - radius, height / 2 - radius
+            ball_vel_x *= -1
+            ball_vel_y *= -1
+        if ball_x <= 0 + radius:
+            ball_x, ball_y = width / 2 - radius, height / 2 - radius
+            ball_vel_x, ball_vel_y = 0.35, 0.35
+
+        # paddles' movements control
+        # if the paddle is about to go over the boundaries, it will stop
+        if right_paddle_y <= 0:
+            right_paddle_y = 0
+        if right_paddle_y >= height - paddle_height:
+            right_paddle_y = height - paddle_height
+        if left_paddle_y <= 0:
+            left_paddle_y = 0
+        if left_paddle_y >= height - paddle_height:
+            left_paddle_y = height - paddle_height
+
+        # ball's collision with the paddles
+        # if the ball hits the left paddle, it will bounce
+        if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+            if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+                ball_x = left_paddle_x + paddle_width
+                ball_vel_x *= -1
+        # if the ball hits the right paddle, it will bounce
+        if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+            if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+                ball_x = right_paddle_x
+                ball_vel_x *= -1
+
+        # draw the elements
+        pygame.draw.circle(wn, pink, (ball_x, ball_y), radius)
+        pygame.draw.rect(wn, aqua, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
+        pygame.draw.rect(wn, aqua, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
+
+        # update the screen
+        pygame.display.update()
